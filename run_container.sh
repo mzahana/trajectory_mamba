746ab80554e7c1e9a85781657208412baf44f6e6
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2024-09-25 19:09:02 +0300

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -3,9 +3,13 @@
 # Variables
 IMAGE_NAME="s_mamba_image"
 CONTAINER_NAME="s_mamba_container"
-REPO_DIR="$(pwd)/repository"
+REPO_DIR="$(pwd)/pkgs"
 APP_DIR="/home/ai/app"
 USER=ai
+
+# Ensure the pkgs directory exists and set appropriate ownership
+mkdir -p $REPO_DIR
+sudo chown -R $(id -u):$(id -g) $REPO_DIR  # Ensure current user owns the directory
 
 # Build the Docker image
 echo "Building Docker image..."
@@ -15,7 +19,7 @@ docker build -t $IMAGE_NAME:latest .
 echo "Running Docker container..."
 docker run --gpus all -it \
     --name $CONTAINER_NAME \
-    -v $REPO_DIR:$APP_DIR/repository \
+    -v $REPO_DIR:$APP_DIR/pkgs \
     -v "$(pwd)/requirements.txt":$APP_DIR/requirements.txt \
     --user $USER \
     $IMAGE_NAME:latest
